d90022f8fda409b042a58655119c28c40219666f
Ilia Naryzhny <phantom@ydn.ru>
2020-05-09 21:52:55 -0700

    Initial commit of java after mvn archetype:generate

---
+++ src/main/java/org/orienteer/jnpm/App.java
@@ -0,0 +1,13 @@
+package org.orienteer.jnpm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
